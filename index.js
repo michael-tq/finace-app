e12c6142e9677d501d4dd806291d5dd7f6be55b3
Michael Qassis <mikeqassis@gmail.com>
2020-01-16 14:41:33 -0800

    added additonal error checking

--- index.js
+++ index.js
@@ -7,13 +7,13 @@ function makeRequest(company, balance, initial, end) {
     let secondDate = end.replace(/-/g, '');
     const beginning = firstDate.slice(0,4) + '-' + firstDate.slice(4,6) + '-' + firstDate.slice(6,8);
     const ending = secondDate.slice(0,4) + '-' + secondDate.slice(4,6) + '-' + secondDate.slice(6,8);
-    const firstURL = searchURL + '?symbol=' + company + '&date=' + beginning + '&' + 'api_token=' + apiKey;
-    const secondURL = searchURL + '?symbol=' + company + '&date=' + ending + '&' + 'api_token=' + apiKey;
-    fetchCall(firstURL, secondURL, company, balance);
+    const urlBeginDate = searchURL + '?symbol=' + company + '&date=' + beginning + '&' + 'api_token=' + apiKey;
+    const urlEndDate = searchURL + '?symbol=' + company + '&date=' + ending + '&' + 'api_token=' + apiKey;
+    fetchPrices(urlBeginDate, urlEndDate, company, balance);
 }
 
-function fetchCall(firstURL, secondURL, company, balance) {
-    fetch(firstURL)
+function fetchPrices(urlBeginDate, urlEndDate, company, balance) {
+    fetch(urlBeginDate)
     .then(response => {
       if (response.ok) {
         return response.json();
@@ -21,7 +21,7 @@ function fetchCall(firstURL, secondURL, company, balance) {
       throw new Error(response.statusText);
     })
     .then(firstResponse => {
-      fetch(secondURL)
+      fetch(urlEndDate)
       .then(response => {
         if (response.ok) {
           return response.json();
@@ -53,20 +53,32 @@ function checkData(company, balance, initial, end) {
   if (company === '') {
     $('.answer').empty();
     $('.errors').html('Please enter a company ticker.');
+    return;
   } else if (balance === '') {
     $('.answer').empty();
     $('.errors').html('Please enter a balance.');
+    return;
+  } else if (isNaN(balance)) {
+    $('.answer').empty();
+    $('.errors').html('Please enter a number for the amount.');
+    return;
   } else if (initial === '' || end === '') {
     $('.answer').empty();
     $('.errors').html('Please enter a date.');
+    return;
   }
-  let firstDate = new Date(initial).getUTCDay();
-  let secondDate = new Date(end).getUTCDay();
-  if (firstDate === 0 || firstDate === 6) {
+  let str1 = new Date(initial);
+  let firstDate = new Date(initial);
+  let secondDate = new Date(end);
+  if (secondDate <= firstDate) {
+    alert('error');
+    return;
+  }
+  if (firstDate.getUTCDay() === 0 || firstDate.getUTCDay() === 6) {
     $('.answer').empty();
     $('.errors').html('Please do not select weekends or holidays. You can use the dropdown to help select a date.');
     return;
-  } else if (secondDate === 0 || secondDate === 6) {
+  } else if (secondDate.getUTCDay() === 0 || secondDate.getUTCDay() === 6) {
     $('.answer').empty();
     $('.errors').html('Please do not select weekends or holidays. You can use the dropdown to help select a date.');
     return;
